e6fa6defb44ec5b7577d185b105eab5086dcf769
강기석 <pyt773924@gmail.com>
2023-08-17 09:40:43 +0900

    초기 패키지 생성

---
+++ src/com/dita/xd/model/UserBean.java
@@ -0,0 +1,37 @@
+package com.dita.xd.model;
+
+public class UserBean {
+    String userId;
+    String password;
+    String email;
+
+    public UserBean(String userId, String password, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
