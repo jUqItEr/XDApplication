c9219c3194a43d6c823051fb0b50f57ec2636219
jUqItEr <103951167+jUqItEr@users.noreply.github.com>
2023-08-21 02:01:35 +0900

    RegisterService 구현

--- src/com/dita/xd/Main.java
+++ src/com/dita/xd/Main.java
@@ -6,15 +6,12 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    LoginFrame frame = new LoginFrame();
-                    frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+        EventQueue.invokeLater(() -> {
+            try {
+                LoginFrame frame = new LoginFrame();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         });
     }
--- src/com/dita/xd/service/implementation/RegisterServiceImpl.java
+++ src/com/dita/xd/service/implementation/RegisterServiceImpl.java
@@ -3,6 +3,5 @@ package com.dita.xd.service.implementation;
 import com.dita.xd.model.UserBean;
 
 public interface RegisterServiceImpl extends ServiceImpl {
-    boolean register(String id, String pwd, String email);
-    boolean register(UserBean bean);
+    boolean register(String id, String pwd, String email, String nickname);
 }
