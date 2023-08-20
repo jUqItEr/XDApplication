279f922d41ec47e76bff6e75d668e103caa0e52c
jUqItEr <103951167+jUqItEr@users.noreply.github.com>
2023-08-21 02:06:34 +0900

    LoginController 구현

--- src/com/dita/xd/controller/LoginController.java
+++ src/com/dita/xd/controller/LoginController.java
@@ -1,7 +1,28 @@
 package com.dita.xd.controller;
 
-public class LoginController {
-    void login() {
+import com.dita.xd.model.UserBean;
+import com.dita.xd.repository.UserRepository;
+import com.dita.xd.service.LoginService;
 
+public class LoginController {
+    private LoginService svc = null;
+    private UserRepository repository = null;
+
+    public LoginController() {
+        svc = new LoginService();
+        repository = UserRepository.getInstance();
+    }
+
+    boolean login(String id, String pwd) {
+        UserBean bean = svc.login(id, pwd);
+
+        if (bean != null) {
+            repository.setLogin(bean);
+        }
+        return bean != null;
+    }
+
+    boolean logout() {
+        return svc.logout(repository);
     }
 }
