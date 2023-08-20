d1bced5467746feca1c39a7c494f835af22b347c
DelynMk2 <dnjs1174@gmail.com>
2023-08-21 03:24:55 +0900

    - LoginController 기능 구현

--- src/com/dita/xd/controller/LoginController.java
+++ src/com/dita/xd/controller/LoginController.java
@@ -1,7 +1,26 @@
 package com.dita.xd.controller;
 
-public class LoginController {
-    void login() {
+import com.dita.xd.model.UserBean;
+import com.dita.xd.repository.UserRepository;
+import com.dita.xd.service.LoginService;
+import com.dita.xd.view.LoginFrame;
 
-    }
-}
+public class LoginController {
+
+    private LoginService loginService;
+    private UserRepository userRepository;
+
+    public LoginController(LoginService loginService, UserRepository userRepository, LoginFrame loginFrame){
+        this.userRepository = userRepository;
+        this.loginService = loginService;
+    }// -- End of cunstructor
+    void login(String id, String pwd) {
+        UserBean bean = loginService.login(id, pwd);
+
+        if (bean != null){
+            userRepository.setLogin(bean);
+        } else{
+
+        }// -- End of if
+    }// -- End of function (login)
+}// -- End of class
