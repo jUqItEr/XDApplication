eb816e1a5b7b44fd7c103ae075e9ec89cd28743b
jUqItEr <103951167+jUqItEr@users.noreply.github.com>
2023-08-21 03:25:58 +0900

    Locale Resource 추가, LoginFrame 테스트

--- src/com/dita/xd/view/LoginFrame.java
+++ src/com/dita/xd/view/LoginFrame.java
@@ -1,19 +1,51 @@
 package com.dita.xd.view;
 
 import com.dita.xd.controller.LoginController;
+import com.dita.xd.view.base.JHintTextField;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 
 public class LoginFrame extends JFrame {
     private LoginController controller = null;
+    private String title;
+
+    private JPanel pnlLogo;
+    private JHintTextField htfId;
+    private JHintTextField htfPwd;
+    private JButton btnLogin;
 
     public LoginFrame() {
-        controller = new LoginController();
+        this.controller = new LoginController();
+        this.title = "XD Login";
+
         /* Initialize components */
         initialize();
     }
 
     private void initialize() {
+        this.setBounds(100, 100, 450, 700);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setTitle(title);
+        this.getContentPane().setLayout(new BorderLayout());
 
+        JPanel pnlTop = new JPanel();
+        pnlTop.setBounds(0, 0, 0, 300);
+
+        JPanel pnlLogin = new JPanel(new BorderLayout());
+
+        htfId = new JHintTextField("아이디");
+        htfPwd = new JHintTextField("비밀번호");
+
+        pnlLogin.add(htfId, BorderLayout.NORTH);
+        pnlLogin.add(htfPwd, BorderLayout.SOUTH);
+
+        this.add(pnlLogin);
+
+
+        this.add(pnlTop, BorderLayout.NORTH);
     }
 }
---
+++ src/com/dita/xd/view/base/JHintTextField.java
@@ -0,0 +1,61 @@
+package com.dita.xd.view.base;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+
+public class JHintTextField extends JTextField {
+    final Font fontDefault;
+    final Font fontGain;
+    final Font fontLost;
+    private String hint;
+
+    public JHintTextField(final String hint) {
+        fontDefault = getFont();
+        fontGain = new Font(fontDefault.getFontName(), Font.PLAIN, fontDefault.getSize());
+        fontLost = new Font(fontDefault.getFontName(), Font.ITALIC, fontDefault.getSize());
+
+        this.setHint(hint);
+        this.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                String text = getText();
+
+                if (text.equals(getHint())) {
+                    setText("");
+                    setFont(fontGain);
+                } else {
+                    setText(text);
+                    setFont(fontGain);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                String text = getText();
+
+                if (text.equals(getHint()) || text.length() == 0) {
+                    setText(getHint());
+                    setFont(fontLost);
+                    setForeground(Color.GRAY);
+                } else {
+                    setText(text);
+                    setFont(fontGain);
+                    setForeground(Color.WHITE);
+                }
+            }
+        });
+    }
+
+    public String getHint() {
+        return hint;
+    }
+
+    public void setHint(String hint) {
+        this.hint = hint;
+        this.setText(hint);
+        this.setFont(fontLost);
+        this.setForeground(Color.GRAY);
+    }
+}
