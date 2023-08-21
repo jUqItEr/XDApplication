fd128f2af347ba07dda372c2a5381cf98f72e770
jUqItEr <pyt773924@gmail.com>
2023-08-21 17:52:32 +0900

    LoginFrame 초안 완성

--- src/main/java/com/dita/xd/Main.java
+++ src/main/java/com/dita/xd/Main.java
@@ -4,12 +4,13 @@ import com.dita.xd.view.LoginFrame;
 import mdlaf.MaterialLookAndFeel;
 import mdlaf.themes.JMarsDarkTheme;
 import mdlaf.themes.MaterialLiteTheme;
+import mdlaf.themes.MaterialOceanicTheme;
 
 import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) throws UnsupportedLookAndFeelException {
-        UIManager.setLookAndFeel(new MaterialLookAndFeel(new MaterialLiteTheme()));
+        UIManager.setLookAndFeel(new MaterialLookAndFeel(new MaterialOceanicTheme()));
 
         SwingUtilities.invokeLater(() -> {
             try {
--- src/main/java/com/dita/xd/view/LoginFrame.java
+++ src/main/java/com/dita/xd/view/LoginFrame.java
@@ -6,26 +6,25 @@ import com.dita.xd.view.locale.LocaleChangeable;
 
 import javax.swing.*;
 import java.awt.*;
-import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class LoginFrame extends JFrame implements LocaleChangeable {
-    private ResourceBundle configBundle;
-    private ResourceBundle localeBundle;
+    private final ResourceBundle configBundle;
+    private final ResourceBundle localeBundle;
     private LoginController controller = null;
     private String title;
 
     private JPanel pnlLogo;
     private JHintTextField htfId;
-    private JHintTextField htfPwd;
+    private JHintTextField htfPassword;
     private JButton btnLogin;
 
     public LoginFrame() {
         this.configBundle = ResourceBundle.getBundle("config", Locale.ROOT);
-        this.localeBundle = ResourceBundle.getBundle("language", Locale.KOREAN);
+        this.localeBundle = ResourceBundle.getBundle("language", Locale.getDefault());
         this.controller = new LoginController();
-        this.title = localeBundle.getString("application.login.title");
+        this.title = localeBundle.getString("login.title");
 
         /* Initialize components */
         initialize();
@@ -39,14 +38,30 @@ public class LoginFrame extends JFrame implements LocaleChangeable {
         this.setTitle(title);
         this.getContentPane().setLayout(null);
 
-        htfId = new JHintTextField("아이디");
-        htfPwd = new JHintTextField("비밀번호");
+        htfId = new JHintTextField(localeBundle.getString("login.htfIdHint"));
+        htfPassword = new JHintTextField(localeBundle.getString("login.htfPasswordHint"));
 
-        htfId.setBounds(75, 400, 300, 40);
-        htfPwd.setBounds(75, 460, 300, 40);
+        htfId.setBounds(75, 300, 300, 40);
+        htfPassword.setBounds(75, 360, 300, 40);
+
+        JLabel lblFindPassword = new JLabel(localeBundle.getString("login.lblFindPassword"));
+        lblFindPassword.setBounds(75, 500, 300, 20);
+        Font lblFont = lblFindPassword.getFont();
+        lblFindPassword.setFont(new Font(lblFont.getFontName(), Font.BOLD, lblFont.getSize()));
+        lblFindPassword.setForeground(Color.BLUE);
+        lblFindPassword.setHorizontalAlignment(SwingConstants.CENTER);
+
+        JButton btnLogin = new JButton(localeBundle.getString("login.btnLogin"));
+        btnLogin.setBounds(75, 420, 300, 30);
+
+        JButton btnRegister = new JButton(localeBundle.getString("login.btnRegister"));
+        btnRegister.setBounds(75, 560, 300, 30);
 
         this.add(htfId);
-        this.add(htfPwd);
+        this.add(htfPassword);
+        this.add(lblFindPassword);
+        this.add(btnLogin);
+        this.add(btnRegister);
     }
 
     @Override
