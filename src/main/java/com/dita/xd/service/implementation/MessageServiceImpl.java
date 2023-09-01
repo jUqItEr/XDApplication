cda61a2f86067ac8a16a26727b5b3f6ef06070b9
jUqItEr <103951167+jUqItEr@users.noreply.github.com>
2023-09-02 00:58:30 +0900

    채팅 서비스 및 컨트롤러 정의

---
+++ src/main/java/com/dita/xd/service/implementation/MessageServiceImpl.java
@@ -0,0 +1,147 @@
+package com.dita.xd.service.implementation;
+
+import com.dita.xd.model.ChatMessageBean;
+import com.dita.xd.service.MessageService;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Vector;
+
+import static com.dita.xd.util.helper.ResultSetExtractHelper.extractChatMessageBean;
+
+public class MessageServiceImpl implements MessageService {
+    private final DBConnectionServiceImpl pool;
+
+    public MessageServiceImpl() {
+        pool = DBConnectionServiceImpl.getInstance();
+    }
+
+    @Override
+    public boolean appendMessage(ChatMessageBean bean) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        String sql = "insert into chat_message_tbl values (null, ?, ?, ?, now(), ?)";
+        boolean flag = false;
+
+        try {
+            conn = pool.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, bean.getContent());
+            pstmt.setInt(2, bean.getChatroomId());
+            pstmt.setString(3, bean.getUserId());
+            pstmt.setString(4, String.valueOf(bean.getReadState()));
+
+            flag = pstmt.executeUpdate() == 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            pool.freeConnection(conn, pstmt);
+        }
+        return flag;
+    }
+
+    @Override
+    public ChatMessageBean getMessage(int recordId) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String sql = "select * from chat_logs_view where id = ?";
+        ChatMessageBean bean = null;
+
+        try {
+            conn = pool.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, recordId);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                bean = extractChatMessageBean(rs);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            pool.freeConnection(conn, pstmt, rs);
+        }
+        return bean;
+    }
+
+    @Override
+    public Vector<ChatMessageBean> getMessages(int chatroomId) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String sql = "select * from chat_logs_view where chatroom_id = ?";
+        Vector<ChatMessageBean> beans = new Vector<>();
+
+        try {
+            conn = pool.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, chatroomId);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                beans.addElement(extractChatMessageBean(rs));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            pool.freeConnection(conn, pstmt, rs);
+        }
+        return beans;
+    }
+
+    @Override
+    public Vector<ChatMessageBean> getMessages(int chatroomId, String userId) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String sql = "select * from chat_logs_view where chatroom_id = ? and user_id = ?";
+        Vector<ChatMessageBean> beans = new Vector<>();
+
+        try {
+            conn = pool.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, chatroomId);
+            pstmt.setString(2, userId);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                beans.addElement(extractChatMessageBean(rs));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            pool.freeConnection(conn, pstmt, rs);
+        }
+        return beans;
+    }
+
+    @Override
+    public Vector<ChatMessageBean> getMessages(int chatroomId, String userId, String targetAt) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String sql = "select * from chat_logs_view where chatroom_id = ? and user_id = ? " +
+                "and date(created_at) = date(?)";
+        Vector<ChatMessageBean> beans = new Vector<>();
+
+        try {
+            conn = pool.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, chatroomId);
+            pstmt.setString(2, userId);
+            pstmt.setString(3, targetAt);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                beans.addElement(extractChatMessageBean(rs));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            pool.freeConnection(conn, pstmt, rs);
+        }
+        return beans;
+    }
+}
--- src/main/java/com/dita/xd/util/helper/ResultSetExtractHelper.java
+++ src/main/java/com/dita/xd/util/helper/ResultSetExtractHelper.java
@@ -1,5 +1,6 @@
 package com.dita.xd.util.helper;
 
+import com.dita.xd.model.ChatMessageBean;
 import com.dita.xd.model.FeedBean;
 import com.dita.xd.model.UserBean;
 
@@ -9,6 +10,21 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 
 public class ResultSetExtractHelper {
+    public static ChatMessageBean extractChatMessageBean(ResultSet rs)
+            throws SQLException {
+        Timestamp beanCreatedAt = rs.getTimestamp("created_at");
+        String beanContent = rs.getString("content");
+        String beanReadState = rs.getString("read_state");
+        String beanUserId = rs.getString("user_id");
+        String beanUserNickname = rs.getString("nickname");
+        String beanProfileImage = rs.getString("profile_image");
+        int beanId = rs.getInt("id");
+        int beanChatRoomId = rs.getInt("chatroom_id");
+
+        return new ChatMessageBean(beanId, beanContent, beanChatRoomId, beanUserId,
+                beanUserNickname, beanProfileImage, beanCreatedAt, beanReadState);
+    }
+
     public static FeedBean extractFeedBean(ResultSet rs) throws SQLException {
         FeedBean bean = new FeedBean();
         Timestamp beanCreatedAt = rs.getTimestamp("created_at");
