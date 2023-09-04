b47ac1fb087de5de1ac9eaef7be9dc316ce059eb
jUqItEr <pyt773924@gmail.com>
2023-09-04 15:05:59 +0900

    DBConnectionServiceImpl 시간대 수정

--- src/main/java/com/dita/xd/service/implementation/DBConnectionServiceImpl.java
+++ src/main/java/com/dita/xd/service/implementation/DBConnectionServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.Vector;
 public class DBConnectionServiceImpl implements Service {
     private Vector connections = new Vector(10);
     private String _driver = "com.mysql.cj.jdbc.Driver",
-            _url = "jdbc:mysql://hxlab.co.kr:13307/xd_chat?characterEncoding=UTF-8&serverTimezone=UTC",
+            _url = "jdbc:mysql://hxlab.co.kr:13307/xd_chat?characterEncoding=UTF-8&serverTimezone=Asia/Seoul",
             _user = "root",
             _password = "dita2414";
 
