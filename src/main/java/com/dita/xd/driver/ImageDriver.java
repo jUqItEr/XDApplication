582ce413f0b6e9e04b4961097fd7c2a66daa0658
jUqItEr <103951167+jUqItEr@users.noreply.github.com>
2023-09-05 00:33:45 +0900

    ImageTransferHelper 정의 및 테스트, 종속성 추가

--- src/main/java/com/dita/xd/driver/ImageDriver.java
+++ src/main/java/com/dita/xd/driver/ImageDriver.java
@@ -19,18 +19,23 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Random;
 
+import static com.dita.xd.util.helper.ImageTransferHelper.downloadImage;
+import static com.dita.xd.util.helper.ImageTransferHelper.uploadImage;
+
 public class ImageDriver extends JFrame {
+    JLabel lblPreview = new JLabel();
+
     public ImageDriver() {
         initialize();
     }
 
     private void initialize() {
         setLayout(new BorderLayout());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(new Dimension(600, 800));
 
         JFileChooser chooser = new JFileChooser();
         JButton btnLoad = new JButton("불러오기");
-        JLabel lblPreview = new JLabel();
 
         btnLoad.addActionListener(e -> {
             int result = chooser.showOpenDialog(this);
@@ -38,87 +43,15 @@ public class ImageDriver extends JFrame {
             if (result == JFileChooser.APPROVE_OPTION) {
                 File selectedFile = chooser.getSelectedFile();
 
-                final String fileServer = "http://hxlab.co.kr:9001/upload";
-
-                HttpClient client = HttpClient.newHttpClient();
-
-                try {
-                    Map<Object, Object> data = new LinkedHashMap<>();
-                    data.put("token", "some-token-value");
-                    data.put("file", selectedFile.toPath());
-
-                    // add extra parameters if needed
-
-                    // Random 256 length string is used as multipart boundary
-                    String boundary = new BigInteger(256, new Random()).toString();
-
-                    HttpRequest request = HttpRequest.newBuilder()
-                            .uri(URI.create(fileServer))
-                            .header("Content-Type", "multipart/form-data;boundary=" + boundary)
-                            .POST(ofMimeMultipartData(data, boundary))
-                            .build();
-
-                    HttpResponse<String> response =
-                            client.send(request, HttpResponse.BodyHandlers.ofString());
-
-                    System.out.println(response.body());
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
+                lblPreview.setIcon(downloadImage(uploadImage(selectedFile)));
             }
         });
-
         add(btnLoad, BorderLayout.NORTH);
         add(lblPreview);
-
-        JLabel lblThumbnail = null;
-        try {
-            URL url = new URL("http://hxlab.co.kr:9001/photo/88247569-fa9c-4837-98b3-3ba0a92f69f6");
-            lblThumbnail = new JLabel(new ImageIcon(url));
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        if (lblThumbnail != null) {
-            add(lblThumbnail);
-        }
     }
 
     public static void main(String[] args) {
         ImageDriver driver = new ImageDriver();
         driver.setVisible(true);
-    }
-
-    public static HttpRequest.BodyPublisher ofMimeMultipartData(Map<Object, Object> data,
-                                                                String boundary) throws IOException {
-        // Result request body
-        java.util.List<byte[]> byteArrays = new ArrayList<>();
-
-        // Separator with boundary
-        byte[] separator = ("--" + boundary + "\r\nContent-Disposition: form-data; name=").getBytes(StandardCharsets.UTF_8);
-
-        // Iterating over data parts
-        for (Map.Entry<Object, Object> entry : data.entrySet()) {
-
-            // Opening boundary
-            byteArrays.add(separator);
-
-            // If value is type of Path (file) append content type with file name and file binaries, otherwise simply append key=value
-            if (entry.getValue() instanceof Path path) {
-                String mimeType = Files.probeContentType(path);
-                byteArrays.add(("\"" + entry.getKey() + "\"; filename=\"" + path.getFileName()
-                        + "\"\r\nContent-Type: " + mimeType + "\r\n\r\n").getBytes(StandardCharsets.UTF_8));
-                byteArrays.add(Files.readAllBytes(path));
-                byteArrays.add("\r\n".getBytes(StandardCharsets.UTF_8));
-            } else {
-                byteArrays.add(("\"" + entry.getKey() + "\"\r\n\r\n" + entry.getValue() + "\r\n")
-                        .getBytes(StandardCharsets.UTF_8));
-            }
-        }
-
-        // Closing boundary
-        byteArrays.add(("--" + boundary + "--").getBytes(StandardCharsets.UTF_8));
-
-        // Serializing as byte array
-        return HttpRequest.BodyPublishers.ofByteArrays(byteArrays);
     }
 }
---
+++ src/main/java/com/dita/xd/util/helper/ImageTransferHelper.java
@@ -0,0 +1,101 @@
+package com.dita.xd.util.helper;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+
+public class ImageTransferHelper {
+    private static final String HOST = "http://hxlab.co.kr:9001";
+    private static final String DOWNLOAD_LINK = HOST + "/photo";
+    private static final String UPLOAD_LINK = HOST + "/upload";
+
+    public static ImageIcon downloadImage(String uuid) {
+        ImageIcon result = null;
+
+        try {
+            URL url = new URL(DOWNLOAD_LINK + "/" + uuid);
+            result = new ImageIcon(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }   // -- End of function
+
+    public static String uploadImage(Path path) {
+        final String boundary = new BigInteger(256, new Random()).toString();
+        final Map<Object, Object> data = new LinkedHashMap<>();
+        String result = null;
+        data.put("token", "some-token-value");
+        data.put("file", path);
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(UPLOAD_LINK))
+                    .header("Content-Type", "multipart/form-data;boundary=" + boundary)
+                    .POST(ofMimeMultipartData(data, boundary))
+                    .build();
+            String responseBody = client
+                    .sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .get();
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) parser.parse(responseBody);
+            result = ((String) jsonObject.get("filename")).split("\\.")[0];
+        } catch (IOException | ExecutionException | InterruptedException | ParseException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }   // -- End of function
+
+    public static String uploadImage(File file) {
+        return uploadImage(file.toPath());
+    }   // -- End of function
+
+    private static HttpRequest.BodyPublisher ofMimeMultipartData(Map<Object, Object> data, String boundary)
+            throws IOException {
+        final byte[] separator = ("--" + boundary + "\r\nContent-Disposition: form-data; name=")
+                .getBytes(StandardCharsets.UTF_8);
+        List<byte[]> byteArrays = new ArrayList<>();
+
+        /* Iterating over data entries */
+        for (Map.Entry<Object, Object> entry : data.entrySet()) {
+            /* Opening boundary */
+            byteArrays.add(separator);
+
+            // If value is type of Path (file) append content type with file name and file binaries,
+            // otherwise simply append key=value
+            if (entry.getValue() instanceof Path path) {
+                String mimeType = Files.probeContentType(path);
+
+                byteArrays.add(("\"" + entry.getKey() + "\"; filename=\"" + path.getFileName()
+                        + "\"\r\nContent-Type: " + mimeType + "\r\n\r\n").getBytes(StandardCharsets.UTF_8));
+                byteArrays.add(Files.readAllBytes(path));
+                byteArrays.add("\r\n".getBytes(StandardCharsets.UTF_8));
+            } else {
+                byteArrays.add(("\"" + entry.getKey() + "\"\r\n\r\n" + entry.getValue() + "\r\n")
+                        .getBytes(StandardCharsets.UTF_8));
+            }   // End of if-else
+        }   // -- End of for-loop
+        // Closing boundary
+        byteArrays.add(("--" + boundary + "--").getBytes(StandardCharsets.UTF_8));
+
+        return HttpRequest.BodyPublishers.ofByteArrays(byteArrays);
+    }   // -- End of function
+}
