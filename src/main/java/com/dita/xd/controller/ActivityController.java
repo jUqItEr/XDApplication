9b74433b16369cf599962c822185170cae8a4d21
jUqItEr <103951167+jUqItEr@users.noreply.github.com>
2023-09-05 23:34:47 +0900

    사용자 검색 기능 구현

--- src/main/java/com/dita/xd/controller/ActivityController.java
+++ src/main/java/com/dita/xd/controller/ActivityController.java
@@ -4,10 +4,17 @@ import com.dita.xd.model.ChatroomBean;
 import com.dita.xd.model.FeedBean;
 import com.dita.xd.model.MediaBean;
 import com.dita.xd.model.UserBean;
+import com.dita.xd.service.implementation.ActivityServiceImpl;
 
 import java.util.Vector;
 
 public class ActivityController {
+    private final ActivityServiceImpl svc;
+
+    public ActivityController() {
+        svc = new ActivityServiceImpl();
+    }
+
 //    public boolean addBookmark(UserBean userBean, FeedBean feedBean);
 //    public boolean addComment(UserBean userBean, FeedBean feedBean, String content, Vector<MediaBean> medium);
 //    public boolean addFeed(UserBean userBean, String content, Vector<MediaBean> medium);
@@ -18,7 +25,6 @@ public class ActivityController {
 //    public boolean exitChatroom(ChatroomBean chatroomBean, UserBean userBean);
 //    public boolean inviteChatroom(ChatroomBean chatroomBean, UserBean userBean);
 //    public boolean setProfile(UserBean userBean);
-//    public boolean search(String searchContent);
 //    public boolean removeBookmark(UserBean userBean, FeedBean feedBean);
 //    public boolean removeFeed(UserBean userBean, FeedBean feedBean);
 //    public boolean removeComment(UserBean userBean, FeedBean feedBean);
@@ -30,6 +36,17 @@ public class ActivityController {
 //    public boolean updateViewer(FeedBean feedBean);
 //    public ChatroomBean createChatroom(String name);
 //    public ChatroomBean getChatroom(int chatroomId);
-//
+
+    public Vector search(String searchContent) {
+        Vector result;
+        char delimiter = searchContent.charAt(0);
+
+        switch (delimiter) {
+            case '@' -> result = svc.searchUser(searchContent);
+            case '#' -> result = svc.searchFeedHashtag(searchContent);
+            default -> result = svc.searchFeed(searchContent);
+        }
+        return result;
+    }
 //    public Vector<UserBean> getChatroomUsers(int chatroomId);
 }
