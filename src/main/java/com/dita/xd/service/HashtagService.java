e70b7e2b703d70786ebad6b9be5878e6bbb6c15e
jUqItEr <pyt773924@gmail.com>
2023-09-05 16:46:18 +0900

    해시태그 기능 추가

--- src/main/java/com/dita/xd/service/HashtagService.java
+++ src/main/java/com/dita/xd/service/HashtagService.java
@@ -1,10 +1,17 @@
 package com.dita.xd.service;
 
+import com.dita.xd.model.FeedBean;
 import com.dita.xd.model.HashtagBean;
 
 import java.util.Vector;
 
 public interface HashtagService extends Service {
+    boolean addFeedHashtag(FeedBean bean, String hashtag);
+    boolean addFeedHashtag(int feedId, String hashtag);
+    boolean addHashtag(String hashtag);
+    boolean removeFeedHashtag(FeedBean bean, String hashtag);
+    boolean removeFeedHashtag(int feedId, String hashtag);
+    boolean removeHashtag(String hashtag);
     int getQuotedHashtagCount(String hashtag);
     Vector<HashtagBean> getRecentHashtags(int limits);
     Vector<HashtagBean> getTrendHashtags(int limits);
--- src/main/java/com/dita/xd/service/implementation/HashtagServiceImpl.java
+++ src/main/java/com/dita/xd/service/implementation/HashtagServiceImpl.java
@@ -1,5 +1,6 @@
 package com.dita.xd.service.implementation;
 
+import com.dita.xd.model.FeedBean;
 import com.dita.xd.model.HashtagBean;
 import com.dita.xd.service.HashtagService;
 
@@ -13,6 +14,112 @@ public class HashtagServiceImpl implements HashtagService {
 
     public HashtagServiceImpl() {
         pool = DBConnectionServiceImpl.getInstance();
+    }
+
+    @Override
+    public boolean addFeedHashtag(FeedBean bean, String hashtag) {
+        boolean result = false;
+
+        if (bean != null) {
+            result = addFeedHashtag(bean.getId(), hashtag);
+        }
+        return result;
+    }
+
+    @Override
+    public boolean addFeedHashtag(int feedId, String hashtag) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        String sql = "insert into feed_hashtag_tbl values (?, ?, now())";
+        boolean flag = false;
+
+        try {
+            conn = pool.getConnection();
+            pstmt = conn.prepareStatement(sql);
+
+            int cnt = pstmt.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            pool.freeConnection(conn, pstmt);
+        }
+        return flag;
+    }
+
+    @Override
+    public boolean addHashtag(String hashtag) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        String sql = "insert into hashtag_tbl select null, ? from dual where not exists(" +
+                "select * from hashtag_tbl where content = ?)";
+        boolean flag = false;
+
+        try {
+            conn = pool.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, hashtag);
+            pstmt.setString(2, hashtag);
+
+            flag = pstmt.executeUpdate() == 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            pool.freeConnection(conn, pstmt);
+        }
+        return flag;
+    }
+
+    @Override
+    public boolean removeFeedHashtag(FeedBean bean, String hashtag) {
+        boolean result = false;
+
+        if (bean != null) {
+            result = removeFeedHashtag(bean.getId(), hashtag);
+        }
+        return result;
+    }
+
+    @Override
+    public boolean removeFeedHashtag(int feedId, String hashtag) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        String sql = "delete from feed_hashtag_table fht join hashtag_tbl ht on fht.hashtag_tbl_id = ht.id " +
+                "where fht.feed_tbl_id = ? and ht.content = ?";
+        boolean flag = false;
+
+        try {
+            conn = pool.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, feedId);
+
+            flag = pstmt.executeUpdate() == 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            pool.freeConnection(conn, pstmt);
+        }
+        return flag;
+    }
+
+    @Override
+    public boolean removeHashtag(String hashtag) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        String sql = "delete from hashtag_tbl where content = ?";
+        boolean flag = false;
+
+        try {
+            conn = pool.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, hashtag);
+
+            flag = pstmt.executeUpdate() == 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            pool.freeConnection(conn, pstmt);
+        }
+        return flag;
     }
 
     @Override
@@ -45,7 +152,7 @@ public class HashtagServiceImpl implements HashtagService {
         Connection conn = null;
         PreparedStatement pstmt = null;
         ResultSet rs = null;
-        String sql = "select ht.* from hashtag_tbl ht join feed_hashtag_tbl fht on ht.id = fht.feed_tbl_id " +
+        String sql = "select ht.* from hashtag_tbl ht join feed_hashtag_tbl fht on ht.id = fht.hashtag_tbl_id " +
                 "order by uploaded_at desc limit ?";
         Vector<HashtagBean> beans = new Vector<>();
 
@@ -74,7 +181,7 @@ public class HashtagServiceImpl implements HashtagService {
         PreparedStatement pstmt = null;
         ResultSet rs = null;
         String sql = "select ht.content as content, count(*) as total_count " +
-                "from hashtag_tbl ht join feed_hashtag_tbl fht on ht.id = fht.feed_tbl_id" +
+                "from hashtag_tbl ht join feed_hashtag_tbl fht on ht.id = fht.hashtag_tbl_id" +
                 "where date(fht.updated_at) = curdate() group by content order by count(*) desc limit ?";
         Vector<HashtagBean> beans = new Vector<>();
 
