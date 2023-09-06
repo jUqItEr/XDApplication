3ae788944b16ec9b45965da51dec371deb5d80cd
jUqItEr <pyt773924@gmail.com>
2023-09-06 17:47:59 +0900

    Hint Offset 수정

--- src/main/java/com/dita/xd/view/base/JXdTextPane.java
+++ src/main/java/com/dita/xd/view/base/JXdTextPane.java
@@ -32,11 +32,13 @@ public class JXdTextPane extends JTextPane {
             return;
         }
         final Graphics2D graphics = (Graphics2D) g;
-        final int offset = 4;
+        final int offset = 2;
 
         graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         graphics.setFont(getFont());
         graphics.drawString(hint, getInsets().left, g.getFontMetrics().getMaxAscent() + offset);
+
+        revalidate();
     }
 
     static class XdDocumentFilter extends DocumentFilter {
