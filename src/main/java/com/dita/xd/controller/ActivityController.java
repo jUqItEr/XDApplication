83b0daf19e5c89aebaa991d9eb2040ee96532f16
jUqItEr <pyt773924@gmail.com>
2023-09-06 15:57:13 +0900

    ActivityController 정의

--- src/main/java/com/dita/xd/controller/ActivityController.java
+++ src/main/java/com/dita/xd/controller/ActivityController.java
@@ -17,27 +17,90 @@ public class ActivityController {
         svc = new ActivityServiceImpl();
     }
 
-//    public boolean addBookmark(UserBean userBean, FeedBean feedBean);
-//    public boolean addComment(UserBean userBean, FeedBean feedBean, String content, Vector<MediaBean> medium);
-//    public boolean addFeed(UserBean userBean, String content, Vector<MediaBean> medium);
-//    public boolean addFeedback(UserBean userBean, FeedBean feedBean);
-//    public boolean addLike(UserBean userBean, FeedBean feedBean);
-//    public boolean addUserBlock(UserBean fromUserBean, UserBean toUserBean);
-//    public boolean addUserFollow(UserBean fromUserBean, UserBean toUserBean);
-//    public boolean exitChatroom(ChatroomBean chatroomBean, UserBean userBean);
-//    public boolean inviteChatroom(ChatroomBean chatroomBean, UserBean userBean);
-//    public boolean setProfile(UserBean userBean);
-//    public boolean removeBookmark(UserBean userBean, FeedBean feedBean);
-//    public boolean removeFeed(UserBean userBean, FeedBean feedBean);
-//    public boolean removeComment(UserBean userBean, FeedBean feedBean);
-//    public boolean removeFeedback(UserBean userBean, FeedBean feedBean);
-//    public boolean removeUserBlock(UserBean fromUserBean, UserBean toUserBean);
-//    public boolean removeUserFollow(UserBean fromUserBean, UserBean toUserBean);
-//    public boolean uploadHeaderImage(UserBean userBean, String address);
-//    public boolean uploadProfileImage(UserBean userBean, String address);
-//    public boolean updateViewer(FeedBean feedBean);
-//    public ChatroomBean createChatroom(String name);
-//    public ChatroomBean getChatroom(int chatroomId);
+    public boolean addBookmark(UserBean userBean, FeedBean feedBean) {
+        return svc.addBookmark(userBean, feedBean);
+    }
+
+    public boolean addComment(UserBean userBean, FeedBean fromBean, FeedBean toBean,
+                              String content, Vector<MediaBean> medium) {
+        return svc.addComment(userBean, fromBean, toBean, content, medium);
+    }
+
+    public boolean addFeed(UserBean userBean, String content, Vector<MediaBean> medium) {
+        return svc.addFeed(userBean, content, medium);
+    }
+
+    public boolean addFeedback(UserBean userBean, FeedBean feedBean) {
+        return svc.addFeedback(userBean, feedBean);
+    }
+
+    public boolean addLike(UserBean userBean, FeedBean feedBean) {
+        return svc.addLike(userBean, feedBean);
+    }
+
+    public boolean addUserBlock(UserBean fromUserBean, UserBean toUserBean) {
+        return svc.addUserBlock(fromUserBean, toUserBean);
+    }
+
+    public boolean addUserFollow(UserBean fromUserBean, UserBean toUserBean) {
+        return svc.addUserFollow(fromUserBean, toUserBean);
+    }
+
+    public boolean exitChatroom(ChatroomBean chatroomBean, UserBean userBean) {
+        return svc.exitChatroom(chatroomBean, userBean);
+    }
+
+    public boolean inviteChatroom(ChatroomBean chatroomBean, UserBean userBean) {
+        return svc.inviteChatroom(chatroomBean, userBean);
+    }
+
+    public boolean setProfile(UserBean userBean) {
+        return svc.setProfile(userBean);
+    }
+
+    public boolean removeBookmark(UserBean userBean, FeedBean feedBean) {
+        return svc.removeBookmark(userBean, feedBean);
+    }
+
+    public boolean removeFeed(UserBean userBean, FeedBean feedBean) {
+        return svc.removeFeed(userBean, feedBean);
+    }
+
+    public boolean removeComment(UserBean userBean, FeedBean fromBean, FeedBean toBean) {
+        return svc.removeComment(userBean, fromBean, toBean);
+    }
+
+    public boolean removeFeedback(UserBean userBean, FeedBean feedBean) {
+        return svc.removeFeedback(userBean, feedBean);
+    }
+
+    public boolean removeUserBlock(UserBean fromUserBean, UserBean toUserBean) {
+        return svc.removeUserBlock(fromUserBean, toUserBean);
+    }
+
+    public boolean removeUserFollow(UserBean fromUserBean, UserBean toUserBean) {
+        return svc.removeUserFollow(fromUserBean, toUserBean);
+    }
+
+    public boolean uploadHeaderImage(UserBean userBean, String address) {
+        return svc.uploadHeaderImage(userBean, address);
+    }
+
+    public boolean uploadProfileImage(UserBean userBean, String address) {
+        return svc.uploadProfileImage(userBean, address);
+    }
+
+    public boolean updateViewer(FeedBean feedBean) {
+        return svc.updateViewer(feedBean);
+    }
+
+    public ChatroomBean createChatroom(String name) {
+        return svc.createChatroom(name);
+    }
+
+    public ChatroomBean getChatroom(int chatroomId) {
+        return svc.getChatroom(chatroomId);
+    }
 
     public int getFollowerCount(String userId) {
         return svc.getFollowerCount(userId);
